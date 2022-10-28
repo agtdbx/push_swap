959e69518e3f228512884aee31f1e8808384ff16
Auguste Deroubaix <aderouba@2A7.42angouleme.fr>
2022-10-28 14:02:48 +0200

    UWU

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:53:37 by aderouba          #+#    #+#             */
-/*   Updated: 2022/10/28 10:53:56 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/10/28 14:01:28 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,78 +25,65 @@ typedef struct s_stack
 	int	len_b;
 }	t_stack;
 
-// push_swap.c
 void	free_and_print_error(t_stack *stacks, char **res);
 void	print_result_and_free(t_stack *stacks, char **res);
 char	**joined_instructions(char **tmp, char **res, int *i);
 char	**optimise_res(char **res);
 int		main(int argc, char **argv);
 
-// params.c
 void	free_params_vars(char *jparams, char **params);
 int		is_valid_params(char **params);
 int		is_in_stack(t_stack *stacks, int nb);
 void	fill_stack(t_stack *stacks, char **params);
 t_stack	check_and_parse_params(int argc, char **argv);
 
-// str_utils.c
 char	*join_params(int argc, char **argv);
 size_t	ft_strlen(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		is_empty_param(int argc, char **argv);
 int		ft_strcmp(char *s1, char *s2);
 
-// str_utils2.c
 char	*ft_strdup(const char *s);
 char	*ft_strjoin_free(char *s1, char *s2);
 char	**add_word(char **tab, char *word);
 char	**ft_split(char const *s, char c);
 
-// nbr_utils.c
 int		ft_atoi(const char *nptr);
 long	ft_atol(const char *nptr);
 int		is_int(char *str);
 int		abs(int value);
 
-// stack_utils.c
 void	add_to_stack(t_stack *stacks, int nb);
 int		*create_and_init_tmp(t_stack *stacks);
 void	standard_number(t_stack *stacks, int **tmp, int *value);
 int		standardization(t_stack *stacks);
 int		get_pos_in_stack(t_stack *stacks, char stack, int value);
 
-// stack_swap.c
 void	swap_a(t_stack *stacks, char ***res);
 void	swap_b(t_stack *stacks, char ***res);
 void	swap_both(t_stack *stacks, char ***res);
 
-// stack_push.c
 void	push_a(t_stack *stacks, char ***res);
 void	push_b(t_stack *stacks, char ***res);
 
-// stack_rotate.c
 void	rotate_a(t_stack *stacks, char ***res);
 void	rotate_b(t_stack *stacks, char ***res);
 void	rotate_both(t_stack *stacks, char ***res);
 
-// stack_reverse_rotate.c
 void	reverse_rotate_a(t_stack *stacks, char ***res);
 void	reverse_rotate_b(t_stack *stacks, char ***res);
 void	reverse_rotate_both(t_stack *stacks, char ***res);
 
-// sort_utils.c
 int		is_sort(t_stack *stacks);
 int		is_part_sort(t_stack *stacks, char s);
 int		is_in_order(t_stack *stacks, char s);
 int		get_lower_in_stack(t_stack *stacks, char s);
 int		all_stack_over(t_stack *stacks, char s, int value);
 
-// little_sort.c
 void	little_aglo_sort(t_stack *stacks, char ***res);
 void	little_aglo(t_stack *stacks, char ***res);
 void	my_little_sort(t_stack *stacks, char ***res);
 
-// sort.c
 int		push_end_a(t_stack *stacks, char ***res);
 void	put_pivot_top(t_stack *stacks, char ***res, int pivot, int pos);
 void	sort_push_a(t_stack *stacks, char ***res);
